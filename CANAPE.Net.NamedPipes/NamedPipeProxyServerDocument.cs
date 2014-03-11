e6340de92452dcb3d56b7cee571052b865f08385
James Forshaw <james.forshaw@contextis.co.uk>
2014-03-11 10:33:49 +0000

    Added start of named pipe code

---
+++ CANAPE.Net.NamedPipes/NamedPipeProxyServerDocument.cs
@@ -0,0 +1,16 @@
+﻿using CANAPE.Documents.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CANAPE.Net.NamedPipes
+{
+    public class NamedPipeProxyServerDocument : FixedProxyDocument
+    {
+        public NamedPipeProxyServerDocument()
+        {
+            this.Client = new NamedPipeProxyClientFactory();
+        }
+    }
+}
